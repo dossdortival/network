ee14c06246b1bdb6b57e565cdba36d75e1a849d6
dossdortival <dossunit@gmail.com>
2025-05-21 13:16:30 +0300

    update models code source

--- network/models.py
+++ network/models.py
@@ -2,34 +2,38 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField("self", symmetrical=False, related_name="followers", blank=True)
+
+    def __str__(self):
+        return f"{self.username}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "following_count": self.following.count(),
+            "followers_count": self.followers.count()
+        }
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
 
+    class Meta:
+        ordering = ["-timestamp"]
+
     def __str__(self):
-        return f"Post by {self.user.username} at {self.timestamp}"
+        return f"Post by {self.author.username} on {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
 
     def serialize(self):
         return {
             "id": self.id,
-            "user": self.user.username,
+            "author": self.author.username,
+            "author_id": self.author.id,
             "content": self.content,
             "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
-            "likes": self.likes.count(),
-            "is_liked": False  
-        }
-
-class Follow(models.Model): 
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
-    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('follower', 'followed')
-
-    def __str__(self):
-        return f"{self.follower.username} follows {self.followed.username}" 
+            "likes_count": self.likes.count(),
+            "liked_by_user": False  
+        } 
