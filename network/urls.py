a593bd6bd767b72917afc2d81be44fe6c72ac7f0
dossdortival <dossunit@gmail.com>
2025-05-21 13:17:13 +0300

    update urls routes

--- network/urls.py
+++ network/urls.py
@@ -8,9 +8,18 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+
+    # API routes
     path("posts", views.posts, name="posts"),
-    path("posts/<int:post_id>", views.post, name="post"),
+    path("posts/<int:page>", views.posts, name="posts_page"),
+    path("posts/new", views.new_post, name="new_post"),
+    path("posts/<int:post_id>/edit", views.edit_post, name="edit_post"),
+    path("posts/<int:post_id>/like", views.like_post, name="like_post"),
+
+    # User profile and following
     path("profile/<str:username>", views.profile, name="profile"),
-    path("following", views.following, name="following"),
-    path("follow/<str:username>", views.follow, name="follow"),        
+    path("profile/<str:username>/<int:page>", views.profile, name="profile_page"),
+    path("profile/<str:username>/follow", views.follow, name="follow"),
+    path("following", views.following_posts, name="following"),
+    path("following/<int:page>", views.following_posts, name="following_page"),       
 ]
